37bb4a9ee4739bb26004623774a550d6265d1498
Marcelo Bicudo <marcelobicudo.silva@gmail.com>
2022-05-24 20:30:46 -0300

    incluindo a classe main do projeto

--- atividade/src/atividade/DAO/CategoryDAO.java
+++
@@ -1,6 +0,0 @@
-package atividade.DAO;
-
-public class CategoryDAO {
-	public int add() {
-		return 0;}
-}
---
+++ atividade/src/atividade/Teste.java
@@ -0,0 +1,7 @@
+package atividade;
+
+public class Teste {
+    public static void main(String[] args) {
+        System.out.println("Aplicação iniciada");
+    }
+}
