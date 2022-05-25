9b5249aa03a80b6ecfdb211862abbd93e3c15770
Marcelo Bicudo <marcelobicudo.silva@gmail.com>
2022-05-24 21:07:30 -0300

    criando método getall no dao da classe peso

--- atividade/src/atividade/DAO/PesoDAO.java
+++ atividade/src/atividade/DAO/PesoDAO.java
@@ -1,15 +1,17 @@
 package atividade.DAO;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import atividade.bean.Peso;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class PesoDAO {
 
     private Connection conexao;
 
-    //TODO - incluir parametro
-    public void cadastrar(/*Peso peso*/) {
+    //TODO - incluir parametros
+    public void cadastrar(Peso peso) {
         PreparedStatement stmt = null;
         DAO dao = new DAO();
 
@@ -17,18 +19,17 @@ public class PesoDAO {
             conexao = dao.getConnection();
             String sql = "INSERT INTO T_PESO(id_peso, id_usuario, dt_pesagem, vl_peso) VALUES (SQ_T_PESO.NEXTVAL, ?, ?, ?, ?)";
             stmt = conexao.prepareStatement(sql);
-            stmt.setString(1, T_PESO.getid_peso());
-            stmt.setString(2, T_PESO.getT_USUARIO_id_usuario());
-            stmt.setDouble(3, T_PESO.getdt_pesagem());
-            stmt.setDouble(4, T_PESO.getvl_peso());
-            java.sql.Date data = new java.sql.Date(colaborador.getdt_pesagemgetdt_pesagem().getTimeInMillis());
-            stmt.setDate(5, data);
+            stmt.setInt(1, peso.getId_peso());
+            stmt.setInt(2, peso.getId_usuario());
+            stmt.setDate(3, peso.getDt_pesagem());
+            stmt.setDouble(4, peso.getVl_peso());
 
             stmt.executeUpdate();
         } catch (SQLException e) {
             e.printStackTrace();
         } finally {
             try {
+                assert stmt != null;
                 stmt.close();
                 conexao.close();
             } catch (SQLException e) {
@@ -37,5 +38,39 @@ public class PesoDAO {
         }
     }
 
-    
+    public List<Peso> getAll() {
+        List<Peso> listPeso = new ArrayList<Peso>();
+        PreparedStatement stmt = null;
+        ResultSet resultSet = null;
+        DAO dao = new DAO();
+
+        try {
+            conexao = dao.getConnection();
+            stmt = conexao.prepareStatement("SELECT * FROM T_PESO");
+            resultSet = dao.getData(stmt);
+
+            while (resultSet.next()) {
+                int id_peso = resultSet.getInt("id_peso");
+                int id_usuario = resultSet.getInt("id_usuario");
+                Date dt_pesagem = resultSet.getDate("dt_pesagem");
+                double vl_peso = resultSet.getDouble("vl_peso");
+
+                Peso peso = new Peso(id_peso, id_usuario, dt_pesagem, vl_peso);
+
+                listPeso.add(peso);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                assert stmt != null;
+                stmt.close();
+                conexao.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return listPeso;
+    }
 }
